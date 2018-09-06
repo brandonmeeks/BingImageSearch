ac9bf3054faa2b44543744dc9a0ac99455485476
Brandon Meeks <brandon.meeks@acadian.com>
2018-09-06 15:44:58 -0500

    no more global variables

--- BingImageSearch/Program.cs
+++ BingImageSearch/Program.cs
@@ -11,23 +11,10 @@ namespace BingImageSearch
 
     class Program
     {
-        // **********************************************
-        // *** Update or verify the following values. ***
-        // **********************************************
-
-        // Replace the accessKey string value with your valid access key.
-        const string path = "../../../key.txt";
-
-        // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
-        // search APIs.  In the future, regional endpoints may be available.  If you
-        // encounter unexpected authorization errors, double-check this value against
-        // the endpoint for your Bing search instance in your Azure dashboard.
-        const string uriBase = "https://api.cognitive.microsoft.com/bing/v7.0/images/search";
 
         static void Main()
         {
-
-            var accessKey = FetchAccessKey();
+            var accessKey = ReadFile("../../../key.txt");
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -38,7 +25,7 @@ namespace BingImageSearch
                 var searchTerm = Console.ReadLine();
                 Console.WriteLine("Searching images for: " + searchTerm);
                 var image = BingImageSearch(searchTerm, accessKey);
-                SaveImage(image);
+                SaveImage(image, "../../../savedImages/picture.jpg");
             }
             else
             {
@@ -51,15 +38,12 @@ namespace BingImageSearch
         ///<summary>
         ///Fetches API access key for use
         /// </summary>
-        static String FetchAccessKey()
+        static string ReadFile(string path)
         {
-            string accessKey;
-            using (StreamReader reader = new StreamReader(path))
+            using (var reader = new StreamReader(path))
             {
-                accessKey = reader.ReadToEnd();
+               return reader.ReadToEnd();
             }
-
-            return accessKey;
         }
 
         /// <summary>
@@ -68,7 +52,7 @@ namespace BingImageSearch
         static byte[] BingImageSearch(string searchQuery, string accessKey)
         {
             // Construct the URI of the search request
-            var uriQuery = uriBase + "?q=" + Uri.EscapeDataString(searchQuery);
+            var uriQuery = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=" + Uri.EscapeDataString(searchQuery);
 
             // Perform the Web request and get the response
             var request = HttpWebRequest.Create(uriQuery);
@@ -79,7 +63,6 @@ namespace BingImageSearch
             //extract first search result's contentUrl and name
             dynamic results = JsonConvert.DeserializeObject(json);
             var contentUrl = results.value[0].contentUrl.ToString();
-            Console.WriteLine(contentUrl);
 
             //Perform second web request to get image response
             request = HttpWebRequest.Create(contentUrl);
@@ -95,15 +78,10 @@ namespace BingImageSearch
         }
 
         //Save image to disk
-        static void SaveImage(byte[] image)
+        static void SaveImage(byte[] image, string path)
         {
-            var filePath = "../../../savedImages/picture.jpg";
-            FileStream fs = File.Create(filePath);
-
-            foreach (var b in image)
-            {
-                fs.WriteByte(b);
-            }
+            var fs = File.Create(path);
+            fs.Write(image, 0, image.Length);
         }
     }
 }
